f7f236d8e8da7958477cca16aa7da1e45fbfbb5d
bhuamancallupe <benny.fhc@gmail.com>
2023-11-14 18:43:33 -0500

    add xxl +xxl media queries for responsive on application

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,13 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-bold text-xl text-gray-800 leading-tight ++xxl:leading-none font-montserrat">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+    <div class="py-10">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 ++xxl:px-12 font-montserrat">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg ++xxl:shadow-2xl">
                 <x-jet-welcome />
             </div>
         </div>
